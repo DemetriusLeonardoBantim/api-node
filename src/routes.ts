590f02c2817413802d71ea99fa40226968f4dc1a
demetriusleonardobantim <demetriusleonardobantim98@hotmail.com>
2021-03-29 09:22:57 -0300

    chore: create route for sendMainController

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveysController } from './controllers/SurveysController';
+import { SendMainController } from './controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
 const userSurveysController = new SurveysController();
+const sendMailController = new SendMainController();
 
 router.post('/users', userController.create);
 router.post('/surveys', userSurveysController.create);
 router.get('/surveys', userSurveysController.show);
+router.post('/sendMail', sendMailController.execute);
+
 export { router };
